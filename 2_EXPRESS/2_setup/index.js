831784cf13b6b2ce9ee9cf6826b642aea82fe755
Igor MM <Igor.mmiguel@eaportal.org>
2023-08-31 15:14:49 -0300

    setup do express

---
+++ 2_EXPRESS/2_setup/index.js
@@ -0,0 +1,9 @@
+// importar o express 
+
+const express = require('express')
+const app = express()
+
+// GET e POST 
+app.get('/', (requicisao, resposta) => {
+    resposta.send('Estou utilizando o Express!')
+})
