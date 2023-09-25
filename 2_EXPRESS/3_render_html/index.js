92f76d91435e9e87adf11304f22ac4e5846bdad3
Igor MM <Igor.mmiguel@eaportal.org>
2023-09-25 13:25:31 -0300

    final aula 2. Render HTML

--- 2_EXPRESS/3_render_html/index.js
+++ 2_EXPRESS/3_render_html/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+const caminhoBase = path.join(__dirname, 'templates')
+
 app.get('/', (requicisao, resposta) => {
-    resposta.send("Estou funcionando!")
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
